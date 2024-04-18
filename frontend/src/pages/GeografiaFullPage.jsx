3cc0937498a10396bfaf7d3b2038106cdd511603
juanrealpe972 <juanrealpe972@gmail.com>
2024-04-17 22:04:00 -0500

    agregando formulario de departamento

--- frontend/src/pages/GeografiaFullPage.jsx
+++ frontend/src/pages/GeografiaFullPage.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import ButtonAtom from "../components/atoms/ButtonAtom";
+import AbrirModalTemplate from "../components/templates/AbrirModalTemplate";
+import ButtonCerrarModalAtom from "../components/atoms/ButtonCerrarModalAtom";
+import RegisterPageDepartamento from "../components/organisms/RegisterPageDepartamento";
 
 function GeografiaFullPage() {
+  const [abrirModalDepartamento, setAbrirModalDepartamento] = useState(false);
+  const [abrirModalMunicipio, setAbrirModalMunicipio] = useState(false);
+  const [abrirModalVereda, setAbrirModalVereda] = useState(false);
+  const [abrirModalTipoVari, setAbrirModalTipoVari] = useState(false);
+
+  const toggleAbrirModalDepartamento = () => {
+    setAbrirModalDepartamento(!abrirModalDepartamento);
+  };
+  const toggleAbrirModalMunicipio = () => {
+    setAbrirModalDepartamento(!abrirModalDepartamento);
+  };
+  const toggleAbrirModalVereda = () => {
+    setAbrirModalDepartamento(!abrirModalDepartamento);
+  };
+  const toggleAbrirModalTipoVari = () => {
+    setAbrirModalDepartamento(!abrirModalDepartamento);
+  };
+
   return (
-    <div>GeografiaFullPage</div>
-  )
+    <div>
+      <ButtonAtom onClick={() => setAbrirModalDepartamento(true)}>
+        Registrar Departamento
+      </ButtonAtom>
+      <ButtonAtom onClick={() => setAbrirModalMunicipio(true)}>
+        Registrar Municipio
+      </ButtonAtom>
+      <ButtonAtom onClick={() => setAbrirModalVereda(true)}>
+        Registrar Vereda
+      </ButtonAtom>
+      <ButtonAtom onClick={() => setAbrirModalTipoVari(true)}>
+        Registrar Tipo variedad
+      </ButtonAtom>
+      {abrirModalDepartamento && (
+        <AbrirModalTemplate>
+          <RegisterPageDepartamento
+            onClose={toggleAbrirModalDepartamento}
+            mode="create"
+          />
+          <ButtonCerrarModalAtom onClose={toggleAbrirModalDepartamento} />
+        </AbrirModalTemplate>
+      )}
+      {abrirModalMunicipio && (
+        <AbrirModalTemplate>
+          <RegisterPageDepartamento
+            onClose={toggleAbrirModalMunicipio}
+            mode="create"
+          />
+          <ButtonCerrarModalAtom onClose={toggleAbrirModalMunicipio} />
+        </AbrirModalTemplate>
+      )}
+      {abrirModalVereda && (
+        <AbrirModalTemplate>
+          <RegisterPageDepartamento
+            onClose={toggleAbrirModalVereda}
+            mode="create"
+          />
+          <ButtonCerrarModalAtom onClose={toggleAbrirModalVereda} />
+        </AbrirModalTemplate>
+      )}
+      {abrirModalTipoVari && (
+        <AbrirModalTemplate>
+          <RegisterPageDepartamento
+            onClose={toggleAbrirModalTipoVari}
+            mode="create"
+          />
+          <ButtonCerrarModalAtom onClose={toggleAbrirModalTipoVari} />
+        </AbrirModalTemplate>
+      )}
+    </div>
+  );
 }
 
-export default GeografiaFullPage
+export default GeografiaFullPage;
