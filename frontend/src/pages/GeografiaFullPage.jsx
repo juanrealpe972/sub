7d4b66261f979eb2741b40bdf00cb5d5cc06f231
juanrealpe972 <juanrealpe972@gmail.com>
2024-04-23 17:35:07 -0500

    agregando tablas de departamento y municipio para que el administrador pueda registrarlos para que los usuarios hagan uso de el

--- frontend/src/pages/GeografiaFullPage.jsx
+++ frontend/src/pages/GeografiaFullPage.jsx
@@ -7,19 +7,13 @@ import RegisterPageMunicipio from "../components/organisms/RegisterPageMunicipio
 import RegisterPageVereda from "../components/organisms/RegisterPageVereda";
 import RegisterPageTipoVariedad from "../components/organisms/RegisterPageTipoVariedad";
 import { Button } from "@nextui-org/react";
+import DepartamentoT from "./DepartamentoT";
+import MunicipioT from "./MunicipioT";
 
 function GeografiaFullPage() {
-  const [abrirModalDepartamento, setAbrirModalDepartamento] = useState(false);
-  const [abrirModalMunicipio, setAbrirModalMunicipio] = useState(false);
   const [abrirModalVereda, setAbrirModalVereda] = useState(false);
   const [abrirModalTipoVari, setAbrirModalTipoVari] = useState(false);
 
-  const toggleAbrirModalDepartamento = () => {
-    setAbrirModalDepartamento(!abrirModalDepartamento);
-  };
-  const toggleAbrirModalMunicipio = () => {
-    setAbrirModalMunicipio(!abrirModalMunicipio);
-  };
   const toggleAbrirModalVereda = () => {
     setAbrirModalVereda(!abrirModalVereda);
   };
@@ -28,37 +22,15 @@ function GeografiaFullPage() {
   };
 
   return (
-    <div className="flex p-10 gap-x-4">
-      <ButtonAtom onClick={() => setAbrirModalDepartamento(true)}>
-        Registrar Departamento
-      </ButtonAtom>
-      <ButtonAtom onClick={() => setAbrirModalMunicipio(true)}>
-        Registrar Municipio
-      </ButtonAtom>
+    <div className="flex px-10 gap-x-4 flex-col">
+      <DepartamentoT />
+      <MunicipioT />
       <ButtonAtom onClick={() => setAbrirModalVereda(true)}>
         Registrar Vereda
       </ButtonAtom>
       <ButtonAtom onClick={() => setAbrirModalTipoVari(true)}>
         Registrar Tipo variedad
       </ButtonAtom>
-      {abrirModalDepartamento && (
-        <AbrirModalTemplate>
-          <RegisterPageDepartamento
-            onClose={toggleAbrirModalDepartamento}
-            mode="create"
-          />
-          <ButtonCerrarModalAtom onClose={toggleAbrirModalDepartamento} />
-        </AbrirModalTemplate>
-      )}
-      {abrirModalMunicipio && (
-        <AbrirModalTemplate>
-          <RegisterPageMunicipio
-            onClose={toggleAbrirModalMunicipio}
-            mode="create"
-          />
-          <ButtonCerrarModalAtom onClose={toggleAbrirModalMunicipio} />
-        </AbrirModalTemplate>
-      )}
       {abrirModalVereda && (
         <AbrirModalTemplate>
           <RegisterPageVereda
